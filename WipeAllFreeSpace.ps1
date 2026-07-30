5323fcad8377c7a0d75f6fc2bd83225881f2e8ae
Benjamin <agentflippy5@users.noreply.github.com>
2026-07-30 12:19:01 -0400

    Added check for PPKG

--- WipeAllFreeSpace.ps1
+++ WipeAllFreeSpace.ps1
@@ -4,11 +4,18 @@
 #This script uses the built in cipher.exe program on Windows to overwrite blank space on all drives. This script on it's own does not delete data; it only makes it more difficult to recover already erased data.
 #This script is embedded in the provisioning package that RIPTIDE uses to reset a system, so that it scrubs all unused space after the reset has completed.
 
-#Get all connected drives
-$Drives = Get-PSDrive -PSProvider 'FileSystem'
+#Check if PPKG file exists (indicator of if script is running pre or post reset)
+if (Test-Path -Path "C:\RIPTIDE.ppkg"){
+    Write-Host "Files indicate the system has not been reset. No action will be taken until RIPTIDE completes system reset."
+} else {
+    #Get all connected drives
+    $Drives = Get-PSDrive -PSProvider 'FileSystem'
 
-foreach($Drive in $drives) {
-    #Scrub each drive attached
-    Write-Host "Wiping " $Drive.Root
-    cipher /w:$Drive.Root
+    foreach($Drive in $drives) {
+        #Scrub each drive attached
+        Write-Host "Wiping " $Drive.Root
+        cipher /w:$Drive.Root
+    }
 }
+
+
